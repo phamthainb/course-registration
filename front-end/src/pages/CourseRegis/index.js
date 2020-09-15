a27038eef3e83eb17acf961091575d5974aba837
sonht1109 <mr.hoang7675@gmail.com>
2020-09-16 00:27:31 +0700

    update routes

---
+++ front-end/src/pages/CourseRegis/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function CourseRegis() {
+    return(
+        <h2>This is a CourseRegis</h2>
+    )
+}
+export default CourseRegis;
---
+++ front-end/src/routes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import CourseRegis from './pages/CourseRegis';
+import Timetable from './pages/Timetable';
+import EditInfoForm from './pages/EditInfoForm';
+
+const routes = [
+    {
+        path: "/home",
+        exact: true,
+        main: ()=> <Home/>
+    },
+    {
+        path: "/about",
+        exact: false,
+        main: ()=> <About/>
+    },
+    {
+        path: "/login",
+        exact: true,
+        main: ()=> <Login/>
+    },
+    {
+        path: "/course-regis",
+        exact: false,
+        main: ()=> <CourseRegis/>
+    },
+    {
+        path: "/timetable",
+        exact: false,
+        main: ()=> <Timetable/>
+    },
+    {
+        path: "/edit-info",
+        exact: false,
+        main: ()=> <EditInfoForm/>
+    }
+]
+
+export default routes;
