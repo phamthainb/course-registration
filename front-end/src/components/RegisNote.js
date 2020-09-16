621bd2cdc1d20349ffc9d8fd62a13f9175473884
sonht1109 <mr.hoang7675@gmail.com>
2020-09-17 00:13:40 +0700

    add course-regis

---
+++ front-end/src/components/RegisNote.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function RegisNote(){
+    return(
+        <div className="regis-note text-center">
+            <span>PG: Practice group</span>
+            <span>Crt: Credits</span>
+            <span>Qtt: Quantity</span>
+            <span>Slot: Slots left</span>
+            <span>Les: Number of lessons</span>
+        </div>
+    )
+}
+
+export default RegisNote;
