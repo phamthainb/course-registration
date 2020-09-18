d9e773095ae30f285698eb39a492e74c812b2edf
phamthainb <phamthai4643@gmail.com>
2020-09-18 15:12:57 +0700

    stash

--- front-end/src/pages/CourseRegis/index.js
+++ front-end/src/pages/CourseRegis/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import RegisControl from '../../components/RegisControl';
-import RegisNote from '../../components/RegisNote';
-import RegisTable from '../../components/RegisTable';
-import './style.css'
+import React from "react";
+import RegisControl from "./RegisControl";
+import RegisNote from "./RegisNote";
+import RegisTable from "./RegisTable";
+import "./style.css";
 
 function CourseRegis() {
-    return(
-        <div className="course-regis">
-            <RegisControl></RegisControl>
-            <RegisTable></RegisTable>
-            <RegisNote></RegisNote>
-        </div>
-    )
+  return (
+    <div className="course-regis">
+      <RegisControl></RegisControl>
+      <RegisTable></RegisTable>
+      <RegisNote></RegisNote>
+    </div>
+  );
 }
 export default CourseRegis;
--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,46 +1,50 @@
-import React from 'react'
-import NewsItem from '../../components/NewsItem';
-import './style.css';
+import React from "react";
+import NewsItem from "./NewsItem";
+import "./style.css";
+
+const news = [
+  {
+    title:
+      "Ngày 14/09/2020:  THÔNG BÁO: v/v ĐỀ NGHỊ  SINH VIÊN RÀ SOÁT CÁC HỌC PHẦN CỦA HỌC KỲ 1 (2020-2021)",
+    content:
+      "Theo kế hoạch đã thông báo, Phòng giáo vụ đã hoàn thành việc tổ chức cho sinh viên các khóa đăng ký học kỳ 1 năm học 2020-2021, đã tiếp nhận, xem xét và xử lý Đơn xin bổ sung, rút bớt học phần của tất cả sinh viên có Đơn.",
+  },
+  {
+    title:
+      "Ngày 28/08/2020: THÔNG BÁO: v/v ĐĂNG KÝ HỌC GHÉP và ĐIỀU CHỈNH ĐĂNG KÝ HỌC KỲ 1 (2020-2021)",
+    content:
+      "Thực hiện kế hoạch đăng ký học ghép (Học lại, học cải thiện điểm) và điều chỉnh đăng ký học kỳ 1 năm học 2020-2021, Phòng giáo vụ thông báo Thời gian và một số điều sinh viên các khóa cần lưu ý khi đăng ký.",
+  },
+  {
+    title:
+      "Thông báo: V/v Cập nhật lịch học online học kỳ hè năm học 2019-2020",
+    content: "",
+  },
+  {
+    title:
+      "Thông báo:  V/v Đăng ký thời khóa biểu học kỳ phụ (hè) năm học 2019 - 2020",
+    content: "",
+  },
+  {
+    title:
+      "Thông báo:  v/v Đăng ký học phần thực tập tốt nghiệp các ngành khối kỹ thuật - khóa 2016",
+    content: "",
+  },
+];
 
 function Home() {
+  const mapNewsItem = news.map((item, index) => {
+    return <NewsItem key={index} newsItem={item} />;
+  });
 
-    const news = [
-        {
-            title: "Ngày 14/09/2020:  THÔNG BÁO: v/v ĐỀ NGHỊ  SINH VIÊN RÀ SOÁT CÁC HỌC PHẦN CỦA HỌC KỲ 1 (2020-2021)",
-            content: "Theo kế hoạch đã thông báo, Phòng giáo vụ đã hoàn thành việc tổ chức cho sinh viên các khóa đăng ký học kỳ 1 năm học 2020-2021, đã tiếp nhận, xem xét và xử lý Đơn xin bổ sung, rút bớt học phần của tất cả sinh viên có Đơn."
-        },
-        {
-            title: "Ngày 28/08/2020: THÔNG BÁO: v/v ĐĂNG KÝ HỌC GHÉP và ĐIỀU CHỈNH ĐĂNG KÝ HỌC KỲ 1 (2020-2021)",
-            content: "Thực hiện kế hoạch đăng ký học ghép (Học lại, học cải thiện điểm) và điều chỉnh đăng ký học kỳ 1 năm học 2020-2021, Phòng giáo vụ thông báo Thời gian và một số điều sinh viên các khóa cần lưu ý khi đăng ký."
-        },
-        {
-            title: "Thông báo: V/v Cập nhật lịch học online học kỳ hè năm học 2019-2020",
-            content: ""
-        },
-        {
-            title: "Thông báo:  V/v Đăng ký thời khóa biểu học kỳ phụ (hè) năm học 2019 - 2020",
-            content: ""
-        },
-        {
-            title: "Thông báo:  v/v Đăng ký học phần thực tập tốt nghiệp các ngành khối kỹ thuật - khóa 2016",
-            content: ""
-        }
-    ]
-
-    const mapNewsItem = news.map((item, index)=>{
-        return(
-            <NewsItem key={index} newsItem={item}></NewsItem>
-        )
-    })
-
-    return(
-        <div>
-            <div className="news-section p-4">
-                <h5 className="mb-3">PTIT news : </h5>
-                {mapNewsItem}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="news-section p-4">
+        <h5 className="mb-3">PTIT news : </h5>
+        {mapNewsItem}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
