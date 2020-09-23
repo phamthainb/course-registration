8030e93a2ac434aee9c56635ce403f980ec63a4e
sonht1109 <mr.hoang7675@gmail.com>
2020-09-23 15:46:54 +0700

    update cart reducer

--- front-end/src/pages/CourseRegis/RegisCartTable.js
+++ front-end/src/pages/CourseRegis/RegisCartTable.js
@@ -1,68 +1,71 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 function RegisCartTable(props){
+    // const cart = [
+    //     {
+    //         code: "BAS1146",
+    //         name: "tieng anh b12",
+    //         crt: 4,
+    //         id: 19,
+    //         pg: {
+    //             id: ''
+    //         }
+    //     },
+    //     {
+    //         code: "INT1313",
+    //         name: "co so du lieu",
+    //         crt: 3,
+    //         id: 6,
+    //         pg: {
+    //             id: 2,
+    //         }
+    //     },
+    //     {
+    //         code: "INT1319",
+    //         name: "he dieu hanh",
+    //         crt: 3,
+    //         id: 9,
+    //         pg: {
+    //             id: 1
+    //         }
+    //     },
+    //     {
+    //         code: "INT1330",
+    //         name: "ki thuat vi xu li",
+    //         crt: 3,
+    //         id: 5,
+    //         pg: {
+    //             id: ''
+    //         }
+    //     },
+    // ]
 
-    const cart = [
-        {
-            code: "BAS1146",
-            name: "tieng anh b12",
-            crt: 4,
-            id: 19,
-            pg: {
-                id: ''
-            }
-        },
-        {
-            code: "INT1313",
-            name: "co so du lieu",
-            crt: 3,
-            id: 6,
-            pg: {
-                id: 2,
-            }
-        },
-        {
-            code: "INT1319",
-            name: "he dieu hanh",
-            crt: 3,
-            id: 9,
-            pg: {
-                id: 1
-            }
-        },
-        {
-            code: "INT1330",
-            name: "ki thuat vi xu li",
-            crt: 3,
-            id: 5,
-            pg: {
-                id: ''
-            }
-        },
-    ]
-
-    const mapToCart = cart.map((item, index)=>{
-        return(
-            <tr key={index}>
-                <td>{index+1}</td>
-                <td>{item.code}</td>
-                <td>{item.name}</td>
-                <td>{item.id}</td>
-                <td>{item.pg.id}</td>
-                <td>{item.crt}</td>
-                <td>{480000 * item.crt}</td>
-                <td>Saved to database</td>
-                <td>
-                    <button className="btn btn-outline-dark">Delete</button>
-                </td>
-            </tr>
-        )
-    })
+    // const mapToCart = cart.map((item, index)=>{
+    //     return(
+    //         <tr key={index}>
+    //             <td>{index+1}</td>
+    //             <td>{item.code}</td>
+    //             <td>{item.name}</td>
+    //             <td>{item.id}</td>
+    //             <td>{item.pg.id}</td>
+    //             <td>{item.crt}</td>
+    //             <td>{480000 * item.crt}</td>
+    //             <td>Saved to database</td>
+    //             <td>
+    //                 <button className="btn btn-outline-dark">Delete</button>
+    //             </td>
+    //         </tr>
+    //     )
+    // })
 
     return(
-        <div className="table-responsive mt-3">
+        <div className="table-responsive mt-5">
             <table className="table table-striped regis-submit-table table-bordered">
                 <thead className="thead-dark">
+                    <tr>
+                        <th colSpan={9}>Chosen Subjects</th>
+                    </tr>
                     <tr>
                         <th>No.</th>
                         <th>Code</th>
@@ -76,7 +79,7 @@ function RegisCartTable(props){
                     </tr>
                 </thead>
                 <tbody>
-                    {mapToCart}
+                    {/* {mapToCart} */}
                     <tr>
                         <td colSpan={5} className="text-center">Total</td>
                         <td>12</td>
@@ -86,7 +89,7 @@ function RegisCartTable(props){
                                 <i className="fa fa-check" aria-hidden="true"></i> Save
                             </button>
                             <button className="btn btn-dark float-right">
-                                <i className="fa fa-trash-o" aria-hidden="true"></i> Delete All
+                                Delete All
                             </button>
                         </td>
                         <td></td>
@@ -97,4 +100,14 @@ function RegisCartTable(props){
     )
 }
 
-export default RegisCartTable;
+const mapState = state=>{
+    return{
+        cart: state.cart
+    }
+}
+
+const mapDispatch = dispatch =>{
+    return{}
+}
+
+export default connect(mapState, mapDispatch)(RegisCartTable);
--- front-end/src/pages/CourseRegis/index.js
+++ front-end/src/pages/CourseRegis/index.js
@@ -1,25 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import RegisCartTable from './RegisCartTable';
-import RegisControl from './RegisControl';
 import RegisNote from './RegisNote';
 import RegisTable from './RegisTable';
 import './style.css'
 
 function CourseRegis() {
 
-    const [chosenSubject, setChosenSubject] = useState({});
-
-    const onShowSubjectList = (option)=>{
-        setChosenSubject(option);
-    }
-
     return(
         <div className="course-regis">
-            <RegisControl onShowSubjectList={onShowSubjectList}></RegisControl>
-            <RegisTable chosenSubject={chosenSubject}></RegisTable>
-            <h5>Chosen subjects</h5>
+
+            <RegisTable></RegisTable>
             <RegisCartTable></RegisCartTable>
             <RegisNote></RegisNote>
+
         </div>
     )
 }
